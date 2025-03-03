f81f9a23bba1c47a41eacf809145c867dac6242c
lyubawa93 <lyuba.n3@gmail.com>
2025-03-03 18:16:44 +0300

    Update Main.java2

--- src/main/java/com/lyubawa93/Main.java
+++ src/main/java/com/lyubawa93/Main.java
@@ -1,6 +1,8 @@
 package com.lyubawa93;
 
 public class Main {
+
+    private static string 
     public static void main(String[] args) {
         System.out.println("Hello, qa.guru!!!!!!!!");
     }
